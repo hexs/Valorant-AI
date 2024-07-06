737a2d3a89f2ebd15913c0626ad23e0dd9070ce0
EK <zxjq97@gmail.com>
2024-07-06 16:52:44 +0700

    use xywhn and resize screenshot

--- play.py
+++ play.py
@@ -36,7 +36,7 @@ def predict(data):
 
     model = YOLO(r'train_yolov8_with_gpu/runs/detect/train3/weights/best.pt')
     # cap = cv2.VideoCapture(r'D:\Python_Projects\valo-ai\videos\VALORANT   2024-07-04 05-27-48.mp4')
-    center = np.array([1920, 1080]) / 2
+    center = np.array([0.5, 0.5])
     t1 = datetime.now()
     t2 = datetime.now()
     time = 0.05
@@ -55,61 +55,46 @@ def predict(data):
             print()
             # print(delta_t)
             with mss.mss() as sct:
-                screenshot = sct.grab(sct.monitors[0])
+                screenshot = sct.grab((x1_, y1_, x2_, y2_), )
             image = np.array(screenshot)
             image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
             # image = cap.read()[1]
             results = model(image)
-            cv2.rectangle(image, (0, 0), (600, 200), (255, 255, 255), -1)
+            cv2.rectangle(image, (0, 0), (600, 100), (255, 255, 255), -1)
 
             boxes = results[0].boxes
-            print(boxes.xywh)  # tensor([[1225.1987,  542.2114,   28.5000,   23.3500]], device='cuda:0')
-            boxes_xywh = boxes.xywh.cpu().numpy()
-            boxes_xyxy = boxes.xyxy.cpu().numpy()
+            print(boxes.xywhn)  # tensor([[0.3384, 0.5006, 0.0160, 0.0301]], device='cuda:0')
+            boxes_xywh = boxes.xywhn.cpu().numpy()
+            boxes_xyxy_px = boxes.xyxy.cpu().numpy()
             conf = boxes.conf
             if conf.tolist():
-                print(boxes_xywh)  # [[     1225.2      542.21        28.5       23.35]]
+                print(boxes_xywh)  # [[    0.33843     0.50057    0.015984     0.03014]]
                 distances = np.sqrt(((boxes_xywh[:, :2] - center) ** 2).sum(axis=1))
-                print('distances', distances)  # list of distance
+                print('distances', distances)  # distances [    0.16157]
                 nearest_index = np.argmin(distances)
                 nearest_box_xywh = boxes_xywh[nearest_index]
-                nearest_box_xyxy = boxes_xyxy[nearest_index]
+                nearest_box_xyxy_px = boxes_xyxy_px[nearest_index]
                 xywh = nearest_box_xywh
-                xyxy = nearest_box_xyxy
-                print(f'xywh = {xywh}')  # xywh = [     1229.9      549.18      64.294      56.323]
+                xyxy_px = nearest_box_xyxy_px
+                print(f'xywh = {xywh}')  # xywh = [    0.33843     0.50057    0.015984     0.03014]
                 x, y, w, h = xywh
-                x1, y1, x2, y2 = [int(x) for x in xyxy]
+                x1_px, y1_px, x2_px, y2_px = [int(x) for x in xyxy_px]
 
                 distance = (x - center[0], y - center[1])
+                print('distance', distance)  # distance (-0.16157126426696777, 0.0005723237991333008)
+                distance = distance * np.array((x2_ - x1_, y2_ - y1_), )
+                print('distance', distance)  # distance [    -310.22     0.61798]
 
+                # px
                 ###########################################################
-                ### y = mx
-                # m = 13 / 20
-                # m = 10 / 20
-                # vx = int(m * distance[0])
-                # vy = int(m * distance[1])
-                ###########################################################
-                ### y = mx - ax^2
-                # m = 8 / 20
-                # a = 0.001
-                # vx = int(m * distance[0] - a * distance[0] ** 2)
-                # vy = int(m * distance[1] - a * distance[0] ** 2)
-                ##########################################################
-                ### y = ax^p + bx
-                # a = 1.11  # 0-2
-                # p = 0.68  # 0-2
-                # b = 0  # 0-1
-                # vx = int((a * distance[0]) ** p + b * distance[0])
-                # vy = int((a * distance[1]) ** p + b * distance[1])
-                #########################################################
                 ### y = b arctan(ax)
                 a = 0.001306
                 b = 455
-                vx = int(b * atan(a * distance[0])) * 3
-                vy = int(b * atan(a * distance[1])) * 3
+                vx_px = int(3 * b * atan(a * distance[0]))
+                vy_px = int(3 * b * atan(a * distance[1]))
                 #########################################################
 
-                print('v', vx, vy)
+                print('v', vx_px, vy_px)
 
                 w_h = w * 0.4 / 2
                 h_h = h / 2.8
@@ -120,15 +105,15 @@ def predict(data):
                 if x - w_h <= center[0] <= x + w_h and y - h_h <= center[1] <= y + h_h:
                     s += '<c>'
                 else:
-                    s += f'<{"+" if vx >= 0 else "-"}{abs(vx):03}{"+" if vy >= 0 else "-"}{abs(vy):03}>'
+                    s += f'<{"+" if vx_px >= 0 else "-"}{abs(vx_px):03}{"+" if vy_px >= 0 else "-"}{abs(vy_px):03}>'
 
                 data['control'] = s
 
-                cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                cv2.rectangle(image, (int(x - w_h / 2), int(y - h_h / 2)),
-                              (int(x + w_h / 2), int(y + h_h / 2)), (0, 255, 0), 1)
+                cv2.rectangle(image, (x1_px, y1_px), (x2_px, y2_px), (0, 0, 255), 1)
+                cv2.rectangle(image, (int((x - w_h / 2) * (x2_ - x1_)), int((y - h_h / 2) * (y2_ - y1_))),
+                              (int((x + w_h / 2) * (x2_ - x1_)), int((y + h_h / 2) * (y2_ - y1_))), (0, 255, 0), 1)
 
-                cv2.putText(image, f'{s}', (100, 100), 1, 4, (255, 0, 0), 4)
+                cv2.putText(image, f'{s}', (10, 60), 1, 4, (255, 0, 0), 4)
                 os.makedirs('img_out', exist_ok=True)
                 cv2.imwrite(datetime.now().strftime('img_out/%H%M%S.png'), image)
 
